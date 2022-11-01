9890b916a168a44b8357c13d0136132ebdb8788c
Jorge Pedroza <sebastian.tobon@corprevenir.com>
2022-11-01 08:38:23 -0500

    commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// /**Te ha llegado una carta ✉️ con todos los regalos que debes preparar. El tema es que es una cadena de texto y es muy difícil de leer 😱. ¡Menos mal que han puesto cada regalo separado por espacio! (aunque ten cuidado, porque al ser niños, igual han colado más espacios de la cuenta)
+
+// Encima nos hemos dado cuenta que algunas palabras vienen con un _ delante de la palabra, por ejemplo _playstation, que significa que está tachado y no se tiene que contar.
+
+// Transforma el texto a un objeto que contenga el nombre de cada regalo y las veces que aparece. Por ejemplo, si tenemos el texto: */
+
+// const carta = 'bici coche balón _playstation bici coche peluche'
+
+// const resultado = carta.split(' ').filter((item,index)=>(item.charAt()!=='_'))
+
+// const respuesta = resultado.reduce((anterior, acumulado)=>{
+    
+// })
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+let ArrayMultidimensional = [[1,2,3],[4,5,6],[7,8,9]];
+
+for(let i=0;i<ArrayMultidimensional.length;i++){
+   for(let j=0;j<ArrayMultidimensional[i].length;j++){
+         console.log(ArrayMultidimensional[i][j])
+   }
+}
